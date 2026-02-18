e3d87a0902dacdb6f88d4932d4afadfaba923579
cioina <alexei_cioina@yahoo.com>
2026-02-18 10:03:10 -0800

    update all

--- tests/vitest-angular/vitest.config.ts
+++ tests/vitest-angular/vitest.config.ts
@@ -1,7 +1,7 @@
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+//import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+//import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { playwright } from '@vitest/browser-playwright';
-import { defineConfig, Plugin } from 'vitest/config';
+import { defineConfig /*, Plugin*/ } from 'vitest/config';
 import aotProject from './src/aot/vitest.project';
 import providersProject from './src/providers/vitest.project';
 import resetTestBedBetweenTestsProject from './src/reset-test-bed-between-tests/vitest.project';
@@ -9,7 +9,7 @@ import resetTestBedBetweenTestsProject from './src/reset-test-bed-between-tests/
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/tests/vitest-angular',
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])] as Plugin[],
+  //plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])] as Plugin[],
   test: {
     watch: false,
     reporters: ['default'],
