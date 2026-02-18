76d8726144b93e64fa8cb61fb36d9b4bdd1fbfac
cioina <alexei_cioina@yahoo.com>
2026-02-18 08:39:33 -0800

    update all

--- tests/vitest-angular/src/providers/providers.spec.ts
+++ tests/vitest-angular/src/providers/providers.spec.ts
@@ -2,6 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { expect, test } from 'vitest';
 import { MY_TOKEN } from './my-token';
 
-test('configure providers with setupTestBed', () => {
+test('configure providers with setupTestBed does not work at this time', () => {
   expect(TestBed.inject(MY_TOKEN)).toBe('My Value');
 });
